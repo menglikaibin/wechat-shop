37bc9597f55273478bb6cdcbb46bde11d496acc0
wukaibin <wukaibin@huluwa.cc>
2019-01-11 17:34:34 +0800

    del

--- web/js/web/account/index.js
+++ web/js/web/account/index.js
@@ -4,10 +4,32 @@ var account_index_ops = {
         this.eventBind();
     },
     eventBind:function () {
+        var that = this;
+
         $(".search").click(function () {
             $(".wrap_search").submit();
+        });
+
+        $(".remove").click(function () {
+            that.ops("remove", $(this).attr("data"));
+        })
+    },
+
+    ops: function(act, uid){
+        $.ajax({
+            url: common_ops.buildWebUrl("/account/ops"),
+            type: "POST",
+            data: {
+                act: act,
+                uid: uid
+            },
+            dataType: "json",
+            success: function (res) {
+
+            }
         })
     }
+
 };
 
 $(document).ready(function () {
